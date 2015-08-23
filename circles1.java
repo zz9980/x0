3c658e7cb94316d7ac90093f6dfb130fc6ba0e49
Bruce Alan Martin <bam@suffolk.li>
2015-08-23 18:52:41 -0400

    Create circles1.java

---
+++ circles1.java
@@ -0,0 +1,22 @@
+//// Wild circles #1.
+
+// GLOBAL VARIABLES //
+float x,y;
+
+// Setup:  screen size, initialization. //
+void setup() {
+  size(500, 500);        // Try changing the width & height.
+  x=  width/2;
+  y=  height/2;
+}
+
+// Next frame:  draw three circles. //
+void draw() {
+  ellipse(x, y, 80, 80);
+  ellipse(x+60, y+60, 40, 40);
+  ellipse(x+90, y+90, 20, 20);
+  // Move them???  
+  x=  x + 2;  y=  y + 1;
+}
+
+
