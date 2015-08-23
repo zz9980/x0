35476f53d83c13708ea10fa86497ffa32b47344a
Bruce Alan Martin <bam@suffolk.li>
2015-08-23 19:50:35 -0400

    Create circles0.java

---
+++ circles0.java
@@ -0,0 +1,11 @@
+//// Wild circles #0.
+
+// Setup:  screen size. //
+void setup() {
+  size(200, 200);                // Try changing the width & height.
+}
+
+// Next frame:  draw circle at (x,y). //
+void draw() {
+  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+}
