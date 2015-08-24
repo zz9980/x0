be94e6c61248100cba919d814ec27b54fe1c91b9
LawnGuyLiberty <liberty@wusb.fm>
2015-08-24 00:26:40 -0400

    Update circles0.java

--- circles0.java
+++ circles0.java
@@ -2,10 +2,10 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(100, 300);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse( 50,150, 40,70 );       // Try changing the (x,y) and size.
 }
