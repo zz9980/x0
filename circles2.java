fe20e3cc49d50e6f920df45b35a43c66df2c7a15
Bruce Alan Martin <bam@suffolk.li>
2015-08-23 20:37:32 -0400

    Update circles2.java

--- circles2.java
+++ circles2.java
@@ -5,20 +5,25 @@ String author=  "Bruce Alan Martin";
 
 // GLOBAL VARIABLES //
 float x,y;
+int h,w;
 
 // Setup:  screen size, initialization. //
 void setup() {
   size(500, 500);
   x=  width/2;
   y=  height/2;
+  w=  80;          // width of ellipse.
+  h=  80;          // height of ellipse
 }
 
 // Next frame. //
 void draw() {
   text( title, 10, 10 );
+  text( subtitle, width/2, 10 );
   text( author, 10, height-10 );
+  text( w+"x"+h, width/2,height-10 );
   //
-  ellipse(x, y, 80, 80);
+  ellipse(x, y, w, h);
 }
 
 
@@ -31,7 +36,9 @@ void mousePressed() {
 
 //  Handle keyboard //
 void keyPressed() {
-  // Erase everything & change colors. //
+  // Erase everything, change size & color. //
   background( random(255), random(255), random(255) );
   fill( random(255), random(255), random(255) );
+  w=  int( random(50,150) );
+  h=  int( random(50,150) );
 }
