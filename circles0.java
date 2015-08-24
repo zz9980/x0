0d0c8600265e773737795d7696ec56cef65d8aa3
Bruce Alan Martin <bam@suffolk.li>
2015-08-23 20:18:29 -0400

    Update circles0.java

--- circles0.java
+++ circles0.java
@@ -5,7 +5,7 @@ void setup() {
   size(200, 200);                // Try changing the width & height.
 }
 
-// Next frame:  draw circle at (x,y). //
+// Next frame:  circle at (x,y). //
 void draw() {
   ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
 }
